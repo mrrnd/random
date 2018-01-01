bd9f2b16996d3799f538cf669dbd1375bfb2b0cf
mrrnd <34711377+mrrnd@users.noreply.github.com>
2018-01-02 03:39:18 +0800

    Add files via upload

--- js/angular-app/config.js
+++ js/angular-app/config.js
@@ -2,7 +2,7 @@ randomApp.config(function($locationProvider, RandomProvider) {
     $locationProvider.html5Mode(false);
     $locationProvider.hashPrefix('!');
 
-    RandomProvider.addLottery('daily', '0xaf4d0e373e44d17439d11e5c312cd62efb603238');
-    RandomProvider.addLottery('weekly', '0xaf4d0e373e44d17439d11e5c312cd62efb603238');
-    RandomProvider.addLottery('monthly', '0xaf4d0e373e44d17439d11e5c312cd62efb603238');
+    RandomProvider.addLottery('daily', '0x40AD13badf747717683905bFA4AC8B9B6f0093fC');
+    RandomProvider.addLottery('weekly', '0x40AD13badf747717683905bFA4AC8B9B6f0093fC');
+    RandomProvider.addLottery('monthly', '0x40AD13badf747717683905bFA4AC8B9B6f0093fC');
 });
