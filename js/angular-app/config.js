797f24219459f29670ae617eb23bcc044dcaf365
mrrnd <34711377+mrrnd@users.noreply.github.com>
2018-01-02 04:12:24 +0800

    Add files via upload

--- js/angular-app/config.js
+++ js/angular-app/config.js
@@ -1,6 +1,8 @@
-randomApp.config(function($locationProvider, RandomProvider) {
+randomApp.config(function($locationProvider, RandomProvider, web3Provider) {
     $locationProvider.html5Mode(false);
     $locationProvider.hashPrefix('!');
+
+    web3Provider.setProviderByAddress('http://wallet.parity.io');
 
     RandomProvider.addLottery('daily', '0x40AD13badf747717683905bFA4AC8B9B6f0093fC');
     RandomProvider.addLottery('weekly', '0x40AD13badf747717683905bFA4AC8B9B6f0093fC');
