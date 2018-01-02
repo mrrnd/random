fa460f8c0168a138be5f6d8a4484424fdf1e4b02
mrrnd <34711377+mrrnd@users.noreply.github.com>
2018-01-02 14:59:23 +0800

    Add files via upload

--- js/angular-app/providers/web3-provider.js
+++ js/angular-app/providers/web3-provider.js
@@ -12,8 +12,10 @@ randomApp.provider('web3', function() {
         },
 
         setProviderByAddress: function(address) {
-            // _provider = new window.Web3.providers.HttpProvider(address);
-            _provider = new Web3.providers.HttpProvider("https://wallet.parity.io/");
+            //provider = new window.Web3.providers.HttpProvider(address);
+            // _provider = new Web3.providers.HttpProvider("http://185.22.62.189:8546");
+            _provider = new Web3.providers.HttpProvider("http://185.22.61.111:8545");
+            // _provider = new Web3.providers.HttpProvider("http://wallet.parity.io");
         },
 
         setDefaultProvider: function() {
