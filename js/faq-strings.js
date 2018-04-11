8a99a11a2ae7eab4e57cc132ba492566953cc9cf
mrrnd <34711377+mrrnd@users.noreply.github.com>
2018-04-11 20:17:10 +0800

    Add files via upload
    FAQ update

---
+++ js/faq-strings.js
@@ -0,0 +1,80 @@
+var faq = {
+    "What is RANDOM": "RANDOM is a lottery based on the Ethereum Blockchain. Its main feature is that RANDOM is " +
+            "the fairest lottery in the world.It was reached thanks to the fact that the process is organised on the basis " +
+            "of the Ethereum Blockchain and with the use of smart contracts, that's why in the conduct of these lottery " +
+            "the human factor is completely excluded.",
+
+    "What platform RANDOM work on": "RANDOM works on a basis of the Ethereum blockchain",
+
+    "Features and differences": "1. Transparency and openness\n" +
+            "2. High percentage of winning probability\n" +
+            "3. Trust and safety (Blockchain)\n" +
+            "4. Prizes in Ethereum\n" +
+            "5. Lack of the human factor\n" +
+            "6. Ease of use\n" +
+            "7. Possibility to check and observe the process in real time\n" +
+            "8. Unlimited time of existence",
+
+    "What the RND token is": "The RANDOM [RND] token acts as \"a lottery ticket\", it is a utility token, " +
+            "its purpose is to be chosen or not by the randomize algorithm, based on Hash-function of the block.\n" +
+            "Each RND token participates in a lottery as a separate \"lottery ticket\".",
+
+    "What the name of your token is": "In the RANDOM environment the RND token is used as \"a lottery ticket\", " +
+            "1 token costs 0,01 ETH",
+
+    "What the cost of 1 RND token is": "Price for 1 RND is 0,01 ETH",
+
+    "How the lottery is performed": "The functionality for conducting lotteries is provided entirely by the use of smart contracts. All currency in ETH which sent to the smart contract is frozen till the end of a lottery. A lottery is performed instantly at the moment when a certain block with the frequency specified in the smart contract appears. A lottery begins with a certain block number and comes to an end on the block which will appear approximately in 1 day (1 week, 1 month for different types of lotteries, respectively).",
+
+    "Prize fund": "Prize fund is formed by 90% of the funds collected during a lottery. For every lottery the prize fund is formed separately.",
+
+    "How many percent of all the tickets win": "In a lottery 4% of the participating tokens win.",
+
+    "Is the contract publicly available?": "You can look at the smart contract at the following link on <a href=\"https://github.com/mrrnd/random\">GITHUB</a>",
+
+    "How to participate": "1. Enter the official site https://mr-r.io/\n2. Choose the type of lottery you are interested in (Dayly, Weekly, Monthly)\n3. In the appeared field copy the contract address and transfer you Ethereum amount to the copied contract address, but not less than 0,01 ETH (the price of 1 RND).\n4. RND tokens are transferred automatically.\n5. Wait for the end of the lottery.\n6. In case of winning the lottery withraw ETH with the help the \"Withdraw\" button.",
+
+    "How to check the amount of RND I bought": "For checking the amount of the purchased RND tokens go to the official RANDOM site https://mr-r.io/ and in the corresponding field enter your Myetherwallet address or follow the instruction for checking the RND tokens amount https://etherscan.io/ given on the official RANDOM site",
+
+    "How to withdraw ETH I won": "At any time after the end of the Daily lottery, the winner can transfer the ETH he or she won from the balance on the smart contract by using the Withdraw function on the official RANDOM site https://mr-r.io/, following the instruction given in the appropriate section.",
+
+    "How much do I get in case I win": "In the case of winning you can get one of Jackpots or a part of the rest of the prize fund which is destributed between the rest of the winning tickets.\nIn the case of repeated participation, chances for winning are increasing in proportion to the number of participations by the formula: 0,04*n, wherein N is the number of participations in the draw. Therefore, the chance for winning at the 10th participation is 40%, and the total winning volume would be twice more than the volume of invested funds during all the time.",
+
+    "What is Jackpot?": "A Jackpot is a bigger winning regarding the total amount of the prize fund. In a lottery 4% of the participating tokens win. Types of Jackpots:\nThe senior Jackpot is 10% of the whole prize fund.\nThe average Jackpot is 4% of the whole prize fund.\nThe younger Jackpot is 1% of the whole prize fund.",
+
+    "Can the RND token be transferred?": "You can move RND token - for this purpose follow the instruction for transferring the RND tokens given on the official RANDOM site (the tokens can be used as a gift, for example).",
+
+    "Who founded RANDOM?": "RANDOM was founded by a group of people who fight for truth, fairness and advantages for everyone. Founders don't reveal their personalities as not everybody likes truth, fairness and advantage for everyone. With the help of the Blockchain technology they challenge greedy organizations which make money off ordinary people.",
+
+    "What types of lotteries are in RANDOM?": "In general RANDOM has 3 types of lotteries located on different smart contracts in the Ethereum blockchain:\n1) Daily\n2) Weekly\n3) Monthly",
+
+    "Which currency can I use to buy \"a lottery ticket\"": "The RND token can be purchased only for Ethereum;",
+
+    "How to become an investor": "RANDOM isn't an investment project. People can get benefit participating in RANDOM.",
+
+    "How to become a partner": "To become an Information partner or a partner as a Marketing / PR agency, go to the relevant section on the official site and submit the application. Your application will be considered and you will get feedback via official RANDOM e-mail.",
+
+    "How can I get in touch with RANDOM": "RANDOM keeps in contact only by its official e-mail. To get in contact with RANDOM go to the section \"Contact RANDOM\" and use the data given on this page.",
+
+    "How are the funds which were collected on lotteries allocated": "90% of the prize fund are distributed between winners, 10% of the funds are transferred to the smart contract for distribution between marketing and support goals.",
+
+    "Will the RND token enter the exchangers": "Enter the exchangers is not provided because RND token don't have any speculative goals. RND is a tool with the help of which every person can participate in the honest and transparent RANDOM lottery obased on the Ethereum Blockchain.",
+
+    "What is \"The last bet\"": "At the end of a lottery (the specified network block is reached), you can make \"The last bet\". This bet will be followed by determination of winners. The balance of each winner on the contract will increase in the amount of the prize. Every participant of the Daily lottery can make \"The last bet\". \nThe participant of a lottery who has made \"The last rate\" is guaranteed from 1 to 5 ETH as compensation for the spent Gas (commission) because the commission when making \"The last bet\" require a higher amount of Gaz.",
+
+    "What is a bet": "A bet is purchasing an RND token (a lottery ticket)",
+
+    "When can I bet": "Betting is allowed from the moment of the lottery start. Each bet creates a new lottery ticket.",
+
+    "What is average probability of winning a lottery which isn't connected with RANDOM?": "Average % of probability of winning a prize in a usual, not connected with the RANDOM lottery - 1/60 000 000 (0.0000016%) which is almost impossible.",
+
+    "Can I accumulate the ETH I won on the RANDOM smart contract": "The winning can be accumulated at the contract for unlimited time and can only be withdrawn by the winner.",
+
+    "Was the Rndom smart contract audited?": "The smart contract is publicly available, anyone can analyze it on github.com or examine the audit by IT companies specialized for this purpose (audit will be published on the RANDOM’s official site in the nearest future).",
+
+    "What do the RANDOM creators do during the lotteries": "The only one task of creators is to do marketing for the project and provide the technical means allowing participants to obtain information and to follow the process.",
+
+    "Where are 10% of the collected during every lottery funds transferred": "10% of the funds are transferred to the smart contract for distribution between marketing and support goals.\nFor activities of RANDOM it is necessary to use promotion channels and also to involve specialists who provide development and technical support of the applications (used for displaying of the process and results of a lottery).",
+
+    "How can I calculate the probability to win": "The probability of winning in a draw is calculated by the following formula: \nwhere X is a number of the participating tokens.\nIn the case of repeated participation, chances for winning are increasing in proportion to the number of participations by the formula: 0,04*n, wherein N is the number of participations in the draw. Therefore, the chance for winning at the 10th participation is 40%, and the total winning volume would be twice more than the volume of invested funds during all the time.\n"
+};
