6f018d80170fd33cfe243356ebc0ebdfef519e36
mrrnd <34711377+mrrnd@users.noreply.github.com>
2018-04-24 16:47:14 +0800

    Add files via upload

---
+++ js/faq-rinvest-strings.js
@@ -0,0 +1,48 @@
+var faqRinvest = {
+    "What is RINVEST": "RINVEST is an investment token of the RANDOM Blockchain lottery.",
+
+    "How can I get RINVEST": "The process of token distribution will be implemented in the form of Initial " +
+            "Coin Offering an opportunity to buy a limited amount of investing tokens.",
+
+    "What can I get from owning RINVEST": "- A holder gets a permanent reward in Ethereum, from every Random " +
+            "lotteriy - 0,001% per 1 RINVEST token.\n" +
+            "- Payments are made automatically with the use of the smart contract without any intervention from the team.\n" +
+            "- Payments are made as long as RINVEST tokens are stored in the wallet of its owner.",
+
+    "Is a Bounty campaign provided": "Bounty campaign is not provided in the RANDOM project, however you can " +
+            "participate in AirDrop. The first AirDrop will be held from 11/04/2018 to 18/04/2018.",
+
+    "How long will I have profit from the RINVEST token": "The remuneration is transfered to the wallet address " +
+            "where the RINVEST token is located permanently. The smart contract, as well as the lottery itself, " +
+            "will work endlessly.",
+
+    "Can the RINVEST be transferred to another person?": "The RINVEST token supports the transfer function. The " +
+            "smart contract will constantly send to a owner's wallet the profits from the RANDOM work. If you decide " +
+            "to transfer / move RINVEST to another purse, the RANDOM smart contract will automatically use a new " +
+            "wallet data to send Ethereum.",
+
+    "Will the RINVEST token be listed on any exchangers": "RINVEST is an ERC-20 token, it can be listed on exchanges.",
+
+    "Total sypply": "The token release is limited. The additional emission isn't provided.",
+
+    "RINVEST tokens during Pre-sale": "500 RINVEST + 500 RINVEST (Bonus)",
+
+    "What is the bonus when buying RINVEST during Pre-sale": "Bonus — 100% (1=2) — 500 RINVEST are provided as a " +
+            "reward for purchases",
+
+    "Number of RINVEST tokens during ICO": "1 500 RINVEST",
+
+    "Pres-sale dates": "18.04.2018 — 18.05.2018 — 1 month",
+
+    "ICO dates": "From 18.05.2018 — for an unlimited period of time",
+
+    "Price for 1 RINVEST token": "Price for 1 RINVEST = 1 ETH",
+
+    "How long will it take to receive RINVEST after purchsing": "The period of token receiving after an exchange " +
+            "is 12 hours.",
+
+    "Soft cap, Hard cap": "Soft cap is 2 000 ETH. Hard cap is not stated",
+
+    "How the collected funds are distributed": "All the funds collected via RINVEST Token sale will be used for " +
+            "marketing and PR purposes for increasing the blockchain-based lottery Random participants number."
+};
